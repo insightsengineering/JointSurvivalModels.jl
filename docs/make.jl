26968117c66d7c1bf682c1acf0e363a2d612186e
Yannik Ammann <ammann.yannik@gmail.com>
2024-02-02 22:45:07 +0100

    Update make.jl
    change deployment name
    
    Signed-off-by: Yannik Ammann <ammann.yannik@gmail.com>

--- docs/make.jl
+++ docs/make.jl
@@ -21,5 +21,5 @@ makedocs(
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
 deploydocs(
-    repo = "github.com/insightsengineering/jointmodels.jl.git",
+    repo = "github.com/insightsengineering/JointModels.jl.git",
 )
